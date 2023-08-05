09011bddb2398ff2583c75de90d358f49e5f01b7
brayanmm6 <martinsbrayan514@gmail.com>
2023-08-05 16:44:53 -0300

    Projeto melhorado com a adição de um painel que guarda advises anteriores em um array. Adicionado modo escuro e um botão que permite o usuario escolher entre mostrar ou ocultar painel de advises anteriores.

--- src/js/index.js
+++ src/js/index.js
@@ -1,7 +1,11 @@
+{
 const diceButton = document.getElementById("dice-button")
 const adviseText = document.querySelector(".advise-text")
 const adviseNumber = document.querySelector(".advise-number")
+const asideLeftContent = document.querySelector(".aside-left-content")
+let previosAdvise = []
 let conter = 0
+let adviseConter = 0
 
 adviseNumber.innerHTML = "Advise"
 adviseText.innerHTML = "Click on the dice to generate a randon advise."
@@ -15,8 +19,40 @@ async function adviseApi () {
 
 diceButton.addEventListener("click", async (e) => {
     e.preventDefault()
-    conter += 1
     let advise = await adviseApi()
+    adviseConter = conter + 1
     adviseText.innerHTML = advise
-    adviseNumber.innerHTML = `Advise #${conter}`
+    adviseNumber.innerHTML = `Advise #${adviseConter}`
+    previosAdvise.push(advise)
+    asideLeftContent.innerHTML += `<p><br>${previosAdvise[conter]}<br></p>`
+    conter += 1
 })
+
+}
+
+{
+    const nighModeInput = document.getElementById("night-mode-button")
+    const backgroundArea = document.querySelector("body")
+    nighModeInput.addEventListener("click", () => {
+        if(nighModeInput.checked){
+            backgroundArea.classList.add("night-mode-background")
+        }else{
+            backgroundArea.classList.remove("night-mode-background")
+        }
+    })
+}
+
+{
+    const showAside = document.getElementById("show-aside")
+    const eyeIcon= document.querySelector(".eye-icon")
+    eyeIcon.classList.add("fas", "fa-eye")
+    showAside.addEventListener("click", () => {
+        if(showAside.checked != true){
+            eyeIcon.classList.remove("fas", "fa-eye")
+            eyeIcon.classList.add("fas", "fa-eye-slash")
+        }else{
+            eyeIcon.classList.remove("fas", "fa-eye-slash")
+            eyeIcon.classList.add("fas", "fa-eye")
+        }
+    })
+}
